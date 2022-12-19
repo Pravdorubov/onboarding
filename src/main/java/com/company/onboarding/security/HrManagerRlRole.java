c4ea254c7babea9dac5bc12efd84fe0c5be2316b
cardiofagus <prav@gmall.com>
2022-12-19 10:40:58 +0300

    full

---
+++ src/main/java/com/company/onboarding/security/HrManagerRlRole.java
@@ -0,0 +1,15 @@
+package com.company.onboarding.security;
+
+import com.company.onboarding.entity.Department;
+import com.company.onboarding.entity.User;
+import io.jmix.security.role.annotation.JpqlRowLevelPolicy;
+import io.jmix.security.role.annotation.RowLevelRole;
+
+@RowLevelRole(name = "HR managers's departments adn users", code = "hr-managers-rl")
+public interface HrManagerRlRole {
+    @JpqlRowLevelPolicy(entityClass = Department.class, where = "{E}.hrManager.id = :current_user_id")
+    void department();
+
+    @JpqlRowLevelPolicy(entityClass = User.class, where = "{E}.department.hrManager.id = :current_user_id")
+    void user();
+}
